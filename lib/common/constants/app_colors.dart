825728396ef04807036fdf8b8f36cc64eeabdce0
EdisonValle26 <andresvalle128@gmail.com>
2025-02-07 19:24:05 -0500

    Primer Commit

---
+++ lib/common/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+  static const List<Color> greeGradient = [
+    Color(0xFF63B5AF),
+    Color(0xFF438883),
+  ];
+  static const Color white = Color(0xFFFFFFFF);
+}
---
+++ lib/features/splash/splash_page.dart
@@ -0,0 +1,28 @@
+import 'package:financeapp/common/constants/app_colors.dart';
+import 'package:financeapp/common/constants/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: AppColors.greeGradient,
+          ),
+        ),
+        //child:  Image.asset('assets/images/man.png')
+        child: Text(
+              'financy',
+              style: AppTextStyles.bigText.copyWith(color: AppColors.white),
+            ),
+      ),
+    );
+  }
+}
